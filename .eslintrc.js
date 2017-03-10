2b01ec3184400defe30a11b0a039dddfcfcf66d5
lexicalunit <lexicalunit@lexicalunit.com>
2017-03-10 17:52:18 -0600

    Adds eslint settings.

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "standard",
+    "installedESLint": true,
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    "globals": {
+      "atom": true,
+    },
+};
